ad3ffbd282eb8b502f8ff86e3f45e455821ee691
Jhon Patrick Masan <68581352+aix-dean@users.noreply.github.com>
2025-11-20 10:16:40 +0800

    Refactor Booking model to use proper nested classes and update toJson to remove nulls

--- lib/models/bookings/types.dart
+++ lib/models/bookings/types.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart' show Timestamp;
+import 'package:cloud_firestore/cloud_firestore.dart' hide Transaction;
 
 import '../products/types.dart';
 import '../transactions/types.dart';
@@ -18,19 +18,229 @@ class ProjectCompliance {
   }
 }
 
+/// Represents a client in a booking.
+class BookingClient {
+  final String companyId;
+  final String id;
+  final String name;
+  final String companyName;
+
+  BookingClient({
+    required this.companyId,
+    required this.id,
+    required this.name,
+    required this.companyName,
+  });
+
+  factory BookingClient.fromJson(Map<String, dynamic> json) {
+    return BookingClient(
+      companyId: json['company_id'],
+      id: json['id'],
+      name: json['name'],
+      companyName: json['company_name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'company_id': companyId,
+      'id': id,
+      'name': name,
+      'company_name': companyName,
+    };
+  }
+}
+
+/// Represents cost details.
+class CostDetails {
+  final double basePrice;
+  final double days;
+  final double discount;
+  final double months;
+  final double otherFees;
+  final double pricePerMonth;
+  final double total;
+  final double vatAmount;
+  final double vatRate;
+
+  CostDetails({
+    required this.basePrice,
+    required this.days,
+    required this.discount,
+    required this.months,
+    required this.otherFees,
+    required this.pricePerMonth,
+    required this.total,
+    required this.vatAmount,
+    required this.vatRate,
+  });
+
+  factory CostDetails.fromJson(Map<String, dynamic> json) {
+    return CostDetails(
+      basePrice: json['basePrice'].toDouble(),
+      days: json['days'].toDouble(),
+      discount: json['discount'].toDouble(),
+      months: json['months'].toDouble(),
+      otherFees: json['otherFees'].toDouble(),
+      pricePerMonth: json['pricePerMonth'].toDouble(),
+      total: json['total'].toDouble(),
+      vatAmount: json['vatAmount'].toDouble(),
+      vatRate: json['vatRate'].toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'basePrice': basePrice,
+      'days': days,
+      'discount': discount,
+      'months': months,
+      'otherFees': otherFees,
+      'pricePerMonth': pricePerMonth,
+      'total': total,
+      'vatAmount': vatAmount,
+      'vatRate': vatRate,
+    };
+  }
+}
+
+/// Represents tax details.
+class Tax {
+  final double percentage;
+  final double taxAmount;
+
+  Tax({
+    required this.percentage,
+    required this.taxAmount,
+  });
+
+  factory Tax.fromJson(Map<String, dynamic> json) {
+    return Tax(
+      percentage: json['percentage'].toDouble(),
+      taxAmount: json['taxAmount'].toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'percentage': percentage,
+      'taxAmount': taxAmount,
+    };
+  }
+}
+
+/// Represents discount details.
+class Discount {
+  final double? percentage;
+  final double? amount;
+  final double discountTotal;
+
+  Discount({
+    this.percentage,
+    this.amount,
+    required this.discountTotal,
+  });
+
+  factory Discount.fromJson(Map<String, dynamic> json) {
+    return Discount(
+      percentage: json['percentage']?.toDouble(),
+      amount: json['amount']?.toDouble(),
+      discountTotal: json['discountTotal'].toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      if (percentage != null) 'percentage': percentage,
+      if (amount != null) 'amount': amount,
+      'discountTotal': discountTotal,
+    };
+  }
+}
+
+/// Represents promos details.
+class Promos {
+  final String quotationId;
+  final bool rated;
+
+  Promos({
+    required this.quotationId,
+    required this.rated,
+  });
+
+  factory Promos.fromJson(Map<String, dynamic> json) {
+    return Promos(
+      quotationId: json['quotation_id'],
+      rated: json['rated'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'quotation_id': quotationId,
+      'rated': rated,
+    };
+  }
+}
+
+/// Represents a requirement.
+class Requirement {
+  final String description;
+  final String fileName;
+  final String fileUrl;
+  final bool required;
+  final String title;
+  final String type;
+  final String uploadStatus;
+
+  Requirement({
+    required this.description,
+    required this.fileName,
+    required this.fileUrl,
+    required this.required,
+    required this.title,
+    required this.type,
+    required this.uploadStatus,
+  });
+
+  factory Requirement.fromJson(Map<String, dynamic> json) {
+    return Requirement(
+      description: json['description'],
+      fileName: json['fileName'],
+      fileUrl: json['fileUrl'],
+      required: json['required'],
+      title: json['title'],
+      type: json['type'],
+      uploadStatus: json['uploadStatus'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'description': description,
+      'fileName': fileName,
+      'fileUrl': fileUrl,
+      'required': required,
+      'title': title,
+      'type': type,
+      'uploadStatus': uploadStatus,
+    };
+  }
+}
+
 /// Represents a booking.
 class Booking {
   final String id;
   final String? cancelReason;
   final String? categoryId;
-  final Map<String, dynamic> client;
+  final BookingClient client;
   final String companyId;
   final String? contract;
   final double cost;
-  final Map<String, dynamic> costDetails;
-  final Map<String, dynamic>? tax;
-  final Map<String, dynamic>? discount;
-  final DateTime? created;
+  final CostDetails costDetails;
+  final Tax? tax;
+  final Discount? discount;
+  final dynamic created;
   final DateTime? endDate;
   final List<String>? mediaOrder;
   final String paymentMethod;
@@ -38,9 +248,9 @@ class Booking {
   final String? productName;
   final String productOwner;
   final String? projectName;
-  final Map<String, dynamic>? promos;
+  final Promos? promos;
   final ProjectCompliance? projectCompliance;
-  final List<Map<String, dynamic>>? requirements;
+  final List<Requirement>? requirements;
   final String reservationId;
   final String sellerId;
   final int? spotNumbers;
@@ -48,16 +258,16 @@ class Booking {
   final String status;
   final double totalCost;
   final String type;
-  final DateTime? updated;
+  final dynamic updated;
   final String userId;
   final String quotationId;
   final String? quotationNumber;
   final bool? isCollectibles;
+  final Product? items;
   final String? url;
   final int? forCensorship;
   final int? forScreening;
   final String? airingCode;
-  final Product? items;
   final Transaction? transaction;
 
   Booking({
@@ -94,11 +304,11 @@ class Booking {
     required this.quotationId,
     this.quotationNumber,
     this.isCollectibles,
+    this.items,
     this.url,
     this.forCensorship,
     this.forScreening,
     this.airingCode,
-    this.items,
     this.transaction,
   });
 
@@ -107,14 +317,14 @@ class Booking {
       id: json['id'],
       cancelReason: json['cancel_reason'],
       categoryId: json['category_id'],
-      client: json['client'],
+      client: BookingClient.fromJson(json['client']),
       companyId: json['company_id'],
       contract: json['contract'],
       cost: json['cost'].toDouble(),
-      costDetails: json['costDetails'],
-      tax: json['tax'],
-      discount: json['discount'],
-      created: json['created'] != null ? (json['created'] as Timestamp).toDate() : null,
+      costDetails: CostDetails.fromJson(json['costDetails']),
+      tax: json['tax'] != null ? Tax.fromJson(json['tax']) : null,
+      discount: json['discount'] != null ? Discount.fromJson(json['discount']) : null,
+      created: json['created'],
       endDate: json['end_date'] != null ? (json['end_date'] as Timestamp).toDate() : null,
       mediaOrder: (json['media_order'] as List<dynamic>?)?.cast<String>(),
       paymentMethod: json['payment_method'],
@@ -122,9 +332,9 @@ class Booking {
       productName: json['product_name'],
       productOwner: json['product_owner'],
       projectName: json['project_name'],
-      promos: json['promos'],
+      promos: json['promos'] != null ? Promos.fromJson(json['promos']) : null,
       projectCompliance: json['projectCompliance'] != null ? ProjectCompliance.fromJson(json['projectCompliance']) : null,
-      requirements: (json['requirements'] as List<dynamic>?)?.cast<Map<String, dynamic>>(),
+      requirements: (json['requirements'] as List<dynamic>?)?.map((e) => Requirement.fromJson(e)).toList(),
       reservationId: json['reservation_id'],
       sellerId: json['seller_id'],
       spotNumbers: json['spot_numbers'],
@@ -132,33 +342,33 @@ class Booking {
       status: json['status'],
       totalCost: json['total_cost'].toDouble(),
       type: json['type'],
-      updated: json['updated'] != null ? (json['updated'] as Timestamp).toDate() : null,
+      updated: json['updated'],
       userId: json['user_id'],
       quotationId: json['quotation_id'],
       quotationNumber: json['quotation_number'],
       isCollectibles: json['isCollectibles'],
+      items: json['items'] != null ? Product.fromJson(json['items']) : null,
       url: json['url'],
       forCensorship: json['for_censorship'],
       forScreening: json['for_screening'],
       airingCode: json['airing_code'],
-      items: json['items'] != null ? Product.fromJson(json['items']) : null,
       transaction: json['transaction'] != null ? Transaction.fromJson(json['transaction']) : null,
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {
+    final map = <String, dynamic>{
       'id': id,
       'cancel_reason': cancelReason,
       'category_id': categoryId,
-      'client': client,
+      'client': client.toJson(),
       'company_id': companyId,
       'contract': contract,
       'cost': cost,
-      'costDetails': costDetails,
-      'tax': tax,
-      'discount': discount,
-      'created': created != null ? Timestamp.fromDate(created!) : null,
+      'costDetails': costDetails.toJson(),
+      'tax': tax?.toJson(),
+      'discount': discount?.toJson(),
+      'created': created,
       'end_date': endDate != null ? Timestamp.fromDate(endDate!) : null,
       'media_order': mediaOrder,
       'payment_method': paymentMethod,
@@ -166,9 +376,9 @@ class Booking {
       'product_name': productName,
       'product_owner': productOwner,
       'project_name': projectName,
-      'promos': promos,
+      'promos': promos?.toJson(),
       'projectCompliance': projectCompliance?.toJson(),
-      'requirements': requirements,
+      'requirements': requirements?.map((e) => e.toJson()).toList(),
       'reservation_id': reservationId,
       'seller_id': sellerId,
       'spot_numbers': spotNumbers,
@@ -176,17 +386,19 @@ class Booking {
       'status': status,
       'total_cost': totalCost,
       'type': type,
-      'updated': updated != null ? Timestamp.fromDate(updated!) : null,
+      'updated': updated,
       'user_id': userId,
       'quotation_id': quotationId,
       'quotation_number': quotationNumber,
       'isCollectibles': isCollectibles,
+      'items': items?.toJson(),
       'url': url,
       'for_censorship': forCensorship,
       'for_screening': forScreening,
       'airing_code': airingCode,
-      'items': items?.toJson(),
       'transaction': transaction?.toJson(),
     };
+    map.removeWhere((key, value) => value == null);
+    return map;
   }
 }
